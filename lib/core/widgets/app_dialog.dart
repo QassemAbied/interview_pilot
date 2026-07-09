79238bb1152ad8955795becf2f0ee3b5155bc1b7
qassem abied <qassemab1@gmail.com>
2026-07-09 18:23:14 +0300

    implement AppDialog widget for core UI

---
+++ lib/core/widgets/app_dialog.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:interview_pilot/core/widgets/app_radius.dart';
+import '../theme/app_colors.dart';
+import '../theme/app_text_styles.dart';
+
+final class AppDialog {
+  const AppDialog._();
+
+  static Future<void> show({
+    required BuildContext context,
+    required String title,
+    required String message,
+    String buttonText = "OK",
+  }) {
+    return showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: AppRadius.br20),
+          title: Text(
+            title,
+            style: AppTextStyle.bold(size: 18, color: AppColors.textPrimary),
+          ),
+          content: Text(
+            message,
+            style: AppTextStyle.regular(
+              size: 14,
+              color: AppColors.textSecondary,
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text(
+                buttonText,
+                style: AppTextStyle.semiBold(
+                  size: 15,
+                  color: AppColors.primary,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
