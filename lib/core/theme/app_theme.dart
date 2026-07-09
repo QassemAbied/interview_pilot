00b917a17ec335229d5e95b1e648476c0398fd24
qassem abied <qassemab1@gmail.com>
2026-07-09 18:24:41 +0300

    implement AppTheme and light theme configuration

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:interview_pilot/core/widgets/app_radius.dart';
+
+import 'app_colors.dart';
+
+abstract class AppTheme {
+  const AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+
+    brightness: Brightness.light,
+
+    scaffoldBackgroundColor: AppColors.scaffold,
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      brightness: Brightness.light,
+    ),
+
+    primaryColor: AppColors.primary,
+
+    dividerColor: AppColors.divider,
+
+    splashFactory: NoSplash.splashFactory,
+
+    highlightColor: Colors.transparent,
+
+    splashColor: Colors.transparent,
+
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      centerTitle: true,
+      backgroundColor: AppColors.scaffold,
+      foregroundColor: AppColors.textPrimary,
+      surfaceTintColor: Colors.transparent,
+    ),
+
+    cardTheme: const CardThemeData(
+      elevation: 0,
+      color: AppColors.card,
+      margin: EdgeInsets.zero,
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.white,
+
+      contentPadding: const EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+
+      enabledBorder: OutlineInputBorder(
+        borderRadius: AppRadius.br16,
+        borderSide: const BorderSide(color: AppColors.border),
+      ),
+
+      focusedBorder: OutlineInputBorder(
+        borderRadius: AppRadius.br16,
+        borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
+      ),
+
+      errorBorder: OutlineInputBorder(
+        borderRadius: AppRadius.br16,
+        borderSide: const BorderSide(color: AppColors.error),
+      ),
+
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: AppRadius.br16,
+        borderSide: const BorderSide(color: AppColors.error, width: 1.5),
+      ),
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        foregroundColor: AppColors.white,
+        minimumSize: const Size(double.infinity, 56),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      ),
+    ),
+
+    progressIndicatorTheme: const ProgressIndicatorThemeData(
+      color: AppColors.primary,
+    ),
+  );
+}
