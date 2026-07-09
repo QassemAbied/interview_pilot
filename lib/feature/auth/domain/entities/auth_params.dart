738dc401ebcd08ec8c8bd73214797fd3a4eb6cd5
qassem abied <qassemab1@gmail.com>
2026-07-09 18:26:03 +0300

    add AuthParams entity for authentication feature

---
+++ lib/feature/auth/domain/entities/auth_params.dart
@@ -0,0 +1,9 @@
+class AuthParams {
+  final String email;
+  final String password;
+  final String? phone;
+  final String? name;
+  final String? image;
+
+  AuthParams(this.email, this.password, this.phone, this.name, this.image);
+}
