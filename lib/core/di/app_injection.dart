9ad79740c42d1cc3364685500e69448a62854f83
qassem abied <qassemab1@gmail.com>
2026-07-09 18:23:30 +0300

    implement `initAppInjection` and register `AppCubit` dependency

---
+++ lib/core/di/app_injection.dart
@@ -0,0 +1,7 @@
+import 'package:get_it/get_it.dart';
+
+import '../../app/app_cubit/app_cubit.dart';
+
+void initAppInjection(GetIt sl) {
+  sl.registerFactory(() => AppCubit(sessionService: sl()));
+}
