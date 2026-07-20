f8b65bd8326c55f0234b0eb9a19683db589e78d0
qassem abied <qassemab1@gmail.com>
2026-07-21 01:24:30 +0300

    register Home, Interview, AI, and Message injection modules

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,10 +1,18 @@
 import 'package:get_it/get_it.dart';
 import 'package:interview_pilot/core/di/app_injection.dart';
+import 'package:interview_pilot/core/di/message_injection.dart';
+import 'ai_injection.dart';
 import 'auth_injection.dart';
+import 'home_injection.dart';
+import 'interview_injection.dart';
 
 GetIt sl = GetIt.instance;
 
 Future<void> setupInjection() async {
   initAuthInjection(sl);
   initAppInjection(sl);
+  initHomeInjection(sl);
+  initInterviewInjection(sl);
+  initAiInjection(sl);
+  messageInjection(sl);
 }
