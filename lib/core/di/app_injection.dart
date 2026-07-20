16ecca3fbbcaa4bd209e944fc190bb10456232ec
qassem abied <qassemab1@gmail.com>
2026-07-21 01:20:58 +0300

    update AppCubit injection with getUserUseCase dependency

--- lib/core/di/app_injection.dart
+++ lib/core/di/app_injection.dart
@@ -3,5 +3,9 @@ import 'package:get_it/get_it.dart';
 import '../../app/app_cubit/app_cubit.dart';
 
 void initAppInjection(GetIt sl) {
-  sl.registerFactory(() => AppCubit(sessionService: sl()));
-}
+  sl.registerFactory(
+        () => AppCubit(
+      sessionService: sl(),
+      getUserUseCase: sl(),
+    ),
+  );}
