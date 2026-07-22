fb11370010b7c1a608c366ccb8d620ef89633a88
qassem abied <qassemab1@gmail.com>
2026-07-23 02:25:16 +0300

    refactor HomeLoading to use InterviewHistoryItemEntity dummy data

--- lib/feature/home/presentation/screen/widget/home_loading.dart
+++ lib/feature/home/presentation/screen/widget/home_loading.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:interview_pilot/core/utils/enum/interview_enum.dart';
 import 'package:skeletonizer/skeletonizer.dart';
-
-import '../../../../interview/domain/entities/interview_entity.dart';
-import '../../../domain/entities/dashboard_entity.dart';
+import '../../../../history/domain/entities/interview_history_item_entity.dart';
 import '../home_body.dart';
 
 class HomeLoading extends StatelessWidget {
@@ -14,31 +11,8 @@ class HomeLoading extends StatelessWidget {
     return Skeletonizer(
       enabled: true,
       child: HomeBody(
-        dashboard: DashboardEntity(
-          totalInterviews: 20,
-          averageScore: 90,
-          bestScore: 97,
-          successRate: 92,
-          recentInterviews: List.generate(
-            3,
-            (index) => InterviewEntity(
-              id: '$index',
-              userId: '',
-              jobTitle: 'Flutter Developer',
-              companyName: 'Google',
-              jobDescription: '',
-              interviewType: InterviewType.behavioral,
-              level: ExperienceLevel.junior,
-              score: 90,
-              duration: 1200,
-              status: InterviewStatus.completed,
-              startedAt: DateTime.now(),
-              endedAt: DateTime.now(),
-              createdAt: DateTime.now(),
-              updatedAt: DateTime.now(),
-            ),
-          ),
-        ),
+        dashboard: InterviewHistoryItemEntity.dummyList
+
       ),
     );
   }
