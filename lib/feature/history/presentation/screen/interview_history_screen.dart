f1dc486e63f963512a3e8e14f804ece5a152b0f8
qassem abied <qassemab1@gmail.com>
2026-07-23 02:25:41 +0300

    implement state-based UI and skeleton loader in InterviewHistoryScreen

--- lib/feature/history/presentation/screen/interview_history_screen.dart
+++ lib/feature/history/presentation/screen/interview_history_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:interview_pilot/feature/history/presentation/screen/widgets/interview_history_skeleton.dart';
 import '../../../../core/di/injection_container.dart';
+import '../../../../core/utils/enum/request_status.dart';
 import '../controller/interview_history_cubit.dart';
+import '../controller/interview_history_state.dart';
 import 'interview_history_body.dart';
 
 class InterviewHistoryScreen extends StatelessWidget {
@@ -11,13 +14,32 @@ class InterviewHistoryScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => sl<InterviewHistoryCubit>()..getInterviewHistory(),
-      child:Scaffold(
-        appBar: AppBar(
-          title: const Text('Interview History'),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Interview History')),
+        body: BlocBuilder<InterviewHistoryCubit, InterviewHistoryState>(
+          builder: (context, state) {
+            if (state.status == RequestStatus.loading) {
+              return InterviewHistorySkeleton();
+            }
+
+            if (state.status == RequestStatus.error) {
+              return Center(
+                child: Text(
+                  state.errorMessage ?? 'Failed to load interview history.',
+                ),
+              );
+            }
+
+            if (state.status == RequestStatus.success) {
+              if (state.interviews.isEmpty) {
+                return const Center(child: Text('No interviews yet.'));
+              }
+              return InterviewHistoryBody(interviewHistory: state.interviews);
+            }
+            return const SizedBox();
+          },
         ),
-        body: const InterviewHistoryBody(),
-      )
+      ),
     );
   }
 }
-
