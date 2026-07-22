7b0576cdda375cf4a423c154f2c836f08f7df966
qassem abied <qassemab1@gmail.com>
2026-07-22 17:55:25 +0300

    register history and interview result dependencies in injection container

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -3,8 +3,10 @@ import 'package:interview_pilot/core/di/app_injection.dart';
 import 'package:interview_pilot/core/di/message_injection.dart';
 import 'ai_injection.dart';
 import 'auth_injection.dart';
+import 'history_injection.dart';
 import 'home_injection.dart';
 import 'interview_injection.dart';
+import 'interview_result_injection.dart';
 
 GetIt sl = GetIt.instance;
 
@@ -15,4 +17,6 @@ Future<void> setupInjection() async {
   initInterviewInjection(sl);
   initAiInjection(sl);
   messageInjection(sl);
+  initInterviewResultInjection(sl);
+  initHistoryInjection(sl);
 }
