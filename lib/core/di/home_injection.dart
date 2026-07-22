3f0ccd18915c4bf034440bd81ae2e4bb7077f533
qassem abied <qassemab1@gmail.com>
2026-07-23 02:25:22 +0300

    refactor home dependency injection and remove `HomeRepository` registration

--- lib/core/di/home_injection.dart
+++ lib/core/di/home_injection.dart
@@ -1,15 +1,8 @@
 import 'package:get_it/get_it.dart';
-
-import '../../feature/home/data/home_repository_impl.dart';
-import '../../feature/home/domain/home_repository.dart';
 import '../../feature/home/domain/use_case/get_dashboard_use_case.dart';
 import '../../feature/home/presentation/controller/home_cubit.dart';
 
 void initHomeInjection(GetIt sl) {
-  sl.registerLazySingleton<HomeRepository>(
-    () => HomeRepositoryImpl(interviewRepository: sl()),
-  );
-
   sl.registerLazySingleton(() => GetDashboardUseCase(sl()));
 
   sl.registerFactory(() => HomeCubit(getDashboardUseCase: sl()));
