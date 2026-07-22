740ee58c91c4b6d52f185626b2d485fb45d4085f
qassem abied <qassemab1@gmail.com>
2026-07-23 02:24:36 +0300

    remove DashboardEntity, HomeRepository, and HomeRepositoryImpl

--- lib/feature/home/domain/home_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import '../../../core/error/Failure.dart';
-import 'entities/dashboard_entity.dart';
-
-abstract class HomeRepository {
-  Future<Either<Failure, DashboardEntity>> getDashboard();
-}
