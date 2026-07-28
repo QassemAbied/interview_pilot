7b7ecd146f437acc56d4fe4ecb73705428ffb994
qassem abied <qassemab1@gmail.com>
2026-07-28 17:28:15 +0300

    add ResumeFileEntity for resume feature

---
+++ lib/feature/resumes/domain/entities/resume_file_entity.dart
@@ -0,0 +1,11 @@
+class ResumeFileEntity {
+  final String name;
+  final String path;
+  final int size;
+
+  const ResumeFileEntity({
+    required this.name,
+    required this.path,
+    required this.size,
+  });
+}
