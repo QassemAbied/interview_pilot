53135baae72d27fdc40658e1f84cf96d6d54f095
qassem abied <qassemab1@gmail.com>
2026-07-29 18:05:26 +0300

    register GetAllInterviewUseCase in home dependency injection

--- lib/core/di/home_injection.dart
+++ lib/core/di/home_injection.dart
@@ -1,9 +1,13 @@
 import 'package:get_it/get_it.dart';
+import '../../feature/home/domain/use_case/get_all_interview_use_case.dart';
 import '../../feature/home/domain/use_case/get_dashboard_use_case.dart';
 import '../../feature/home/presentation/controller/home_cubit.dart';
 
 void initHomeInjection(GetIt sl) {
   sl.registerLazySingleton(() => GetDashboardUseCase(sl()));
+  sl.registerLazySingleton(() => GetAllInterviewUseCase(sl()));
 
-  sl.registerFactory(() => HomeCubit(getDashboardUseCase: sl()));
+  sl.registerFactory(
+    () => HomeCubit(getDashboardUseCase: sl(), getAllInterviewUseCase: sl()),
+  );
 }
