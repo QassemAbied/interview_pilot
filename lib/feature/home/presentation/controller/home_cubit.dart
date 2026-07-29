5bdf1f8052eac1414775ce1009ce24c7f5b787e9
qassem abied <qassemab1@gmail.com>
2026-07-29 18:04:38 +0300

    implement getAllInterview for home feature

--- lib/feature/home/presentation/controller/home_cubit.dart
+++ lib/feature/home/presentation/controller/home_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:interview_pilot/feature/home/domain/use_case/get_all_interview_use_case.dart';
 
 import '../../../../core/helpers/base_use_case.dart';
 import '../../../../core/utils/enum/request_status.dart';
@@ -6,9 +7,11 @@ import '../../domain/use_case/get_dashboard_use_case.dart';
 import 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
+  final GetAllInterviewUseCase getAllInterviewUseCase;
   final GetDashboardUseCase getDashboardUseCase;
 
-  HomeCubit({required this.getDashboardUseCase}) : super(const HomeState());
+
+  HomeCubit({required this.getDashboardUseCase, required this.getAllInterviewUseCase}) : super(const HomeState());
 
   Future<void> getDashboard() async {
     emit(state.copyWith(status: RequestStatus.loading));
@@ -27,6 +30,26 @@ class HomeCubit extends Cubit<HomeState> {
       (interviews) {
         emit(
           state.copyWith(interviews: interviews, status: RequestStatus.success),
+        );
+      },
+    );
+  }
+
+  Future<void> getAllInterview() async {
+    emit(state.copyWith(status: RequestStatus.loading));
+    final result = await getAllInterviewUseCase(const NoParams());
+    result.fold(
+      (failure) {
+        emit(
+          state.copyWith(
+            status: RequestStatus.error,
+            errorMessage: failure.message,
+          ),
+        );
+        },
+      (interviews) {
+        emit(
+          state.copyWith(allInterview: interviews, status: RequestStatus.success),
         );
       },
     );
--- lib/feature/home/presentation/controller/home_state.dart
+++ lib/feature/home/presentation/controller/home_state.dart
@@ -1,23 +1,28 @@
 import '../../../../core/helpers/base_state.dart';
 import '../../../../core/utils/enum/request_status.dart';
 import '../../../history/domain/entities/interview_history_item_entity.dart';
+import '../../../interview/domain/entities/interview_entity.dart';
 
 class HomeState extends BaseState {
   final List<InterviewHistoryItemEntity>? interviews;
+  final List<InterviewEntity>?allInterview;
 
   const HomeState({
     this.interviews = const [],
+    this.allInterview = const [],
     super.status,
     super.errorMessage,
   });
 
   HomeState copyWith({
     List<InterviewHistoryItemEntity>? interviews,
+    List<InterviewEntity>?allInterview,
     RequestStatus? status,
     String? errorMessage,
   }) {
     return HomeState(
       interviews: interviews ?? this.interviews,
+      allInterview: allInterview ?? this.allInterview,
       status: status ?? this.status,
       errorMessage: errorMessage,
     );
