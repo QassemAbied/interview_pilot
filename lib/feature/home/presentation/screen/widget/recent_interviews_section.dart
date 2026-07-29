e0c09aeb857b0df4e2f0d968c1f35c5eead3b100
qassem abied <qassemab1@gmail.com>
2026-07-29 18:07:18 +0300

    update RecentInterviewsSection to support navigation and new interview entities

--- lib/feature/home/presentation/screen/widget/recent_interviews_section.dart
+++ lib/feature/home/presentation/screen/widget/recent_interviews_section.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../../../../../core/constants/app_spac.dart';
+import '../../../../../core/routing/route_names.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/app_text_styles.dart';
 import '../../../../../core/widgets/spacing.dart';
 import '../../../../history/domain/entities/interview_history_item_entity.dart';
+import '../../../../interview/domain/entities/interview_entity.dart';
 import 'recent_interview_item.dart';
 
 class RecentInterviewsSection extends StatelessWidget {
-  const RecentInterviewsSection({super.key, required this.interviews});
+  const RecentInterviewsSection({super.key, required this.interviews, this.interview});
 
-  final List<InterviewHistoryItemEntity> interviews;
+  final List<InterviewHistoryItemEntity>? interview;
+  final List<InterviewEntity> interviews;
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +26,29 @@ class RecentInterviewsSection extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
-                  'Recent Interviews',
-                  style: AppTextStyle.bold(
-                    size: 20,
-                    color: AppColors.textPrimary,
-                  ),
+                Row(
+                  children: [
+                    Text(
+                      'Recent Interviews',
+                      style: AppTextStyle.bold(
+                        size: 20,
+                        color: AppColors.textPrimary,
+                      ),
+                    ),
+                    const Spacer(),
+                    InkWell(
+                      onTap: (){
+                        context.pushNamed(RouteNames.allInterview);
+                      },
+                      child: Text(
+                        'View All',
+                        style: AppTextStyle.regular(
+                          size: 14,
+                          color: AppColors.primary,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
                 verticalSpace(AppSpacing.s16),
               ],
@@ -39,7 +60,9 @@ class RecentInterviewsSection extends StatelessWidget {
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.only(bottom: AppSpacing.s16),
-                child: RecentInterviewItem(interview: interviews[index]),
+                child: RecentInterviewItem(
+                    interviews: interview![index],
+                    interview: interviews[index]),
               );
             },
             separatorBuilder: (_, _) => const SizedBox.shrink(),
