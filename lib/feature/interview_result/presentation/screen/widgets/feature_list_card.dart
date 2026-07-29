359759603e830fd235fec6984792287ec8af4a87
qassem abied <qassemab1@gmail.com>
2026-07-29 18:04:00 +0300

    refactor FeatureListCard to use AppResultSection widget

--- lib/feature/interview_result/presentation/screen/widgets/feature_list_card.dart
+++ lib/feature/interview_result/presentation/screen/widgets/feature_list_card.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:interview_pilot/core/widgets/result_Section.dart';
 import '../../../../../core/theme/app_colors.dart';
 import '../../../../../core/theme/app_text_styles.dart';
-import '../../../../../core/widgets/app_card.dart';
 import '../../../../../core/widgets/spacing.dart';
 
 class FeatureListCard extends StatelessWidget {
@@ -21,53 +20,37 @@ class FeatureListCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppCard(
+    return AppResultSection(
+      title: title,
+      icon: icon,
+      color: color,
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Icon(icon, color: color),
+        children: items
+            .map(
+              (item) => Padding(
+                padding: const EdgeInsets.only(bottom: 14),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(Icons.check_circle_rounded, color: color, size: 20),
 
-              horizontalSpace(8),
+                    horizontalSpace(12),
 
-              Text(
-                title,
-                style: AppTextStyle.bold(
-                  size: 18,
-                  color: AppColors.textPrimary,
-                ),
-              ),
-            ],
-          ),
-
-          verticalSpace(20),
-
-          ...items.map(
-            (item) => Padding(
-              padding: const EdgeInsets.only(bottom: 14),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Icon(Icons.check_circle_rounded, color: color, size: 20),
-
-                  horizontalSpace(12),
-
-                  Expanded(
-                    child: Text(
-                      item,
-                      style: AppTextStyle.regular(
-                        size: 15,
-                        color: AppColors.textPrimary,
-                        height: 1.5,
+                    Expanded(
+                      child: Text(
+                        item,
+                        style: AppTextStyle.regular(
+                          size: 15,
+                          color: AppColors.textPrimary,
+                          height: 1.5,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ),
-        ],
+            )
+            .toList(),
       ),
     );
   }
