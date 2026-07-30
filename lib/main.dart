eb9734b55e54a0b16a14eef8c75bcc31cfe082c9
qassem abied <qassemab1@gmail.com>
2026-07-30 16:59:07 +0300

    configure Firebase and Fastlane for Android distribution

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:interview_pilot/app/app.dart';
 import 'core/di/injection_container.dart';
 import 'core/services/supabase_service/supabase_init.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await dotenv.load(fileName: '.env');
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await SupAbaseInitialization.init();
   await setupInjection();
 
   runApp(const App());
 }
-
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import audio_session
 import connectivity_plus
 import file_picker
 import file_selector_macos
+import firebase_core
 import flutter_tts
 import just_audio
 import printing
@@ -26,6 +27,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
