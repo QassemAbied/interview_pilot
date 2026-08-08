81f00b1861d0c9a1b9f7766641c663f9df695ef0
qassem abied <qassemab1@gmail.com>
2026-08-08 18:45:57 +0300

    update UpdateUserUseCase to use UpdateParams

--- lib/feature/auth/domain/use_case/update_user_use_case.dart
+++ lib/feature/auth/domain/use_case/update_user_use_case.dart
@@ -2,15 +2,15 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/error/Failure.dart';
 import '../../../../core/helpers/base_use_case.dart';
 import '../auth_repository.dart';
-import '../entities/auth_params.dart';
+import '../entities/update_params.dart';
 
-class UpdateUserUseCase implements UseCase<Unit, AuthParams> {
+class UpdateUserUseCase implements UseCase<Unit, UpdateParams> {
   final AuthRepository repository;
 
   UpdateUserUseCase(this.repository);
 
   @override
-  Future<Either<Failure, Unit>> call(AuthParams params) async {
+  Future<Either<Failure, Unit>> call(UpdateParams params) async {
     return await repository.updateUser(authParams: params);
   }
 }
