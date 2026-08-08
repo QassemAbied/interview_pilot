24bdc00cd7a766ea91d79aa7441b8d204973a106
qassem abied <qassemab1@gmail.com>
2026-08-08 18:48:51 +0300

    implement UpdateParams entity for auth feature

---
+++ lib/feature/auth/domain/entities/update_params.dart
@@ -0,0 +1,9 @@
+class UpdateParams {
+  final String? name;
+  final String? image;
+  final String? bio;
+  final String? language;
+
+
+  UpdateParams(  this.name, this.image, this.bio, this.language);
+}
