1134ef24207446204cd47ffd4bcfddb40f3bad67
qassem abied <qassemab1@gmail.com>
2026-08-08 18:48:35 +0300

    implement SettingsSectionModel for settings feature

---
+++ lib/feature/settings/presentation/screen/models/settings_section_model.dart
@@ -0,0 +1,11 @@
+import 'setting_item.dart';
+
+class SettingsSectionModel {
+  final String title;
+  final List<SettingItem> items;
+
+  const SettingsSectionModel({
+    required this.title,
+    required this.items,
+  });
+}
